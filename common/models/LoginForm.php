3d10a3177d263f3917a3a1e34ff664522b4fe48f
Pawan Korotane <pawan.korotane@shazam.com>
2016-03-04 11:35:20 +0000

    Added admin login function

--- common/models/LoginForm.php
+++ common/models/LoginForm.php
@@ -75,4 +75,15 @@ class LoginForm extends Model
 
         return $this->_user;
     }
+    
+    
+    public function loginAdmin()
+    {
+        if(($this->validate()) && $this->getUser()->role_id >= ValueHelper::getRoleValue('Admin')
+                && $this->getUser()->status == ValueHelper::getStatusValue('Active')){
+           return Yii::$app->user->login($this->getUser(), $this->rememberMe ? 3600 * 24 * 30 : 0);
+        }else{
+            throw new NotFoundHttpException('You Shall Not Pass.');
+        }
+    }
 }
